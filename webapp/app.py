1184bc08199e8aa1b6e771ddc0da5e1e98756601
arnaldorusso <arnaldorusso@gmail.com>
2014-09-18 18:59:12 -0300

    Trying Flask in a hard Way

--- webapp/app.py
+++ webapp/app.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 # python 3 import support
-from __future__ import absolute_import
-
 from flask import Flask
-from flask_flatpages import FlatPages
-from flask_frozen import Freezer
 
 app = Flask(__name__)
-app.config.from_pyfile('settings.py')
-pages = FlatPages(app)
-freezer = Freezer(app)
+
+
+@app.route("/")
+def hello_world():
+    return "hello Wolrd!! <strong>I\'m learning Flask</strong>", 200
+
+app.run()
